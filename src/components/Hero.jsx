e215ba11e2954e8bd757fb8765d3b178d1332f57
Caio Basaglia <41481398+caioob@users.noreply.github.com>
2026-09-05 17:14:24 -0300

    Style manual changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero({ profile }) {
             aria-hidden="true"
             className="mt-4 flex justify-end lg:absolute lg:top-0 lg:left-1/2 lg:mt-0 lg:ml-16 lg:-translate-x-1/2 lg:-translate-y-4"
           >
-            <img src={profile.logo} alt="" className="h-auto w-36 sm:w-48 lg:w-logo" />
+            <img src={profile.logo} alt="" className="h-auto w-36 sm:w-48 lg:w-logo ml-150 p-2" />
           </div>
         )}
       </Container>
